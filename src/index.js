8a77ba064896dc15b534878342503385e6048bc4
abt-kameoka <k.kameoka@altbridge-tech.jp>
2022-03-29 11:33:30 +0900

    0329a

--- src/index.js
+++ src/index.js
@@ -5,16 +5,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", function (req, res) {
-//   res.send("Hello World!");
-// });
-
 app.post("/", function (req, res) {
   const data = req.body.events[0].message;
   console.log("req.body", data);
   res.send("api: OK");
 });
-
-// app.listen(3000, function () {});
 
 process.env.NOW_REGION ? (module.exports = app) : app.listen(PORT);
